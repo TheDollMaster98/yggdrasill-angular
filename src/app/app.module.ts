2f2f5bb79fbccab4ec91014aaf737cbbc2333c19
TheDollMaster98 <lorisdalsanto@hotmail.it>
2023-11-03 11:15:28 +0100

    install angularfirebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,10 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { HttpClientModule } from '@angular/common/http';
 
+// angular firebase
+import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { GlitchClockComponent } from './components/glitch-clock/glitch-clock.component';
 import { DashboardComponent } from './components/pages/dashboard/dashboard.page';
@@ -17,6 +21,18 @@ import { InstagramLinkComponent } from './components/icon-component/instagram-li
 import { LinkedinLinkComponent } from './components/icon-component/linkedin-link/linkedin-link.component';
 import { TiktokLinkComponent } from './components/icon-component/tiktok-link/tiktok-link.component';
 import { YoutubeLinkComponent } from './components/icon-component/youtube-link/youtube-link.component';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyA754pPbe9v4WR5yVmhehmOulTGJTi-HIQ',
+  authDomain: 'yggdrasill-project.firebaseapp.com',
+  databaseURL:
+    'https://yggdrasill-project-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'yggdrasill-project',
+  storageBucket: 'yggdrasill-project.appspot.com',
+  messagingSenderId: '780116050986',
+  appId: '1:780116050986:web:e63a4c6e4c11efb747ef53',
+  measurementId: 'G-0SB1BB90X4',
+};
 
 @NgModule({
   declarations: [
@@ -32,7 +48,13 @@ import { YoutubeLinkComponent } from './components/icon-component/youtube-link/y
     TiktokLinkComponent,
     YoutubeLinkComponent,
   ],
-  imports: [BrowserModule, NgbModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+    NgbModule,
+    HttpClientModule,
+  ],
   providers: [DashboardService],
   bootstrap: [AppComponent],
 })
