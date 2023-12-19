4ee1ed1a98bdb219f166f84a4c49dce77e2e8361
TheDollMaster98 <lorisdalsanto@hotmail.it>
2023-12-19 17:25:05 +0100

    fix grafici

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiService } from './service/api.service';
+import { BackgroundColorClass } from './models/enum';
 
 const mainBgRoutes = ['/dashboard'];
 const routesBgRoutes = ['/dashboard'];
@@ -32,13 +33,29 @@ export class AppComponent {
 
   getMainBackgroundColorClass() {
     return mainBgRoutes.includes(this.router.url)
-      ? 'page-body'
-      : 'page-body-white';
+      ? BackgroundColorClass.DayMode
+      : BackgroundColorClass.NightMode;
   }
 
   getRoutesBackgroundColorClass() {
     return routesBgRoutes.includes(this.router.url)
-      ? 'page-body'
-      : 'page-body-white';
+      ? BackgroundColorClass.DayMode
+      : BackgroundColorClass.NightMode;
+  }
+
+  isDayMode(): boolean {
+    const currentHour = new Date().getHours();
+    return currentHour >= 6 && currentHour < 18;
+  }
+
+  shouldApplyMinHeight(): boolean {
+    // Aggiungi la logica per determinare se applicare o meno l'altezza minima
+    // Ad esempio, verifica la lunghezza del contenuto o altri criteri
+
+    // Esempio: applica l'altezza minima solo se il contenuto è più corto di 100 pixel
+    const contentElement = document.querySelector('.main-content'); // Assuming you have a class named 'main-content' on the content element
+    const contentHeight = contentElement ? contentElement.clientHeight : 0;
+
+    return contentHeight < 100;
   }
 }
---
+++ src/app/models/enum.ts
@@ -0,0 +1,8 @@
+// enums.ts
+
+export enum BackgroundColorClass {
+  DayMode = 'day-mode',
+  NightMode = 'night-mode',
+}
+
+// Altri enums o costanti possono essere aggiunti qui, se necessario
