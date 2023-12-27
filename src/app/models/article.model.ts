228be966d2f071edae5c01312024bd69d53c72ea
TheDollMaster98 <lorisdalsanto@hotmail.it>
2023-12-27 17:34:40 +0100

    update article model

--- src/app/models/article.model.ts
+++ src/app/models/article.model.ts
@@ -1,5 +1,15 @@
 // article.model.ts
 export interface Article {
+  id?: string;
+  author?: string;
+  genre?: string;
+  articleTitle?: string;
+  articleContent?: string;
+  publishDate?: string;
+}
+
+export class articleData {
+  id?: string;
   author?: string;
   genre?: string;
   articleTitle?: string;
@@ -10,16 +20,3 @@ export interface Article {
 export interface ArticleList {
   articols: Record<string, Article>;
 }
-/**
-L'interfaccia `ArticleList` definisce una struttura dati per rappresentare una lista di articoli. Ecco una breve sintesi:
-
-- `export interface ArticleList`: Questa linea indica che stai esportando un'interfaccia chiamata `ArticleList` per renderla accessibile in altri moduli o file TypeScript.
-
-- `articols`: È un membro di `ArticleList` e rappresenta un oggetto contenente articoli. Ogni articolo è identificato da una chiave di tipo stringa e ha un valore associato di tipo `Article`.
-
-- `Record<string, Article>`: Questo specifica che l'oggetto `articols` deve avere chiavi di tipo stringa e i valori associati a queste chiavi devono essere di tipo `Article`.
-
-- `Article`: Si riferisce a un'altra interfaccia o tipo che dovrebbe definire la struttura di un singolo articolo, comprese le proprietà come `articleContent`, `articleTitle`, `author`, etc.
-
-In conclusione, `ArticleList` è un modo di rappresentare una lista di articoli, ciascuno con una struttura definita da `Article`. 
-*/
