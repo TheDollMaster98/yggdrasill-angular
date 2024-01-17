d4462eb861ef951d2744fdad9c5a27db77d31e65
TheDollMaster98 <lorisdalsanto@hotmail.it>
2024-01-17 15:56:43 +0100

    funcntion module

--- src/app/firebase/firebase.module.ts
+++ src/app/firebase/firebase.module.ts
@@ -8,18 +8,22 @@ import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { AngularFireModule } from '@angular/fire/compat';
 import { getStorage, provideStorage } from '@angular/fire/storage';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    AngularFirestoreModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
-    // auth firebase.
-    AngularFireAuthModule,
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
-    provideFirestore(() => getFirestore()),
-    provideStorage(() => getStorage()),
+    // AngularFireAuthModule, //auth
+    provideAuth(() => getAuth()), //auth
+    // AngularFirestoreModule, //cloud firestore
+    provideFirestore(() => getFirestore()), //cloud firestore
+    provideStorage(() => getStorage()), // cloud storage
+    provideDatabase(() => getDatabase()), //realtime db
+    provideFunctions(() => getFunctions()), //functions
   ],
 })
 export class FirebaseModule {}
