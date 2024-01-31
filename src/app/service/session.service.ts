8539927591bc323e28dc2fc0a0d260054c348efe
TheDollMaster98 <lorisdalsanto@hotmail.it>
2024-01-31 16:59:50 +0100

    create session service

---
+++ src/app/service/session.service.ts
@@ -0,0 +1,18 @@
+// session.service.ts
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SessionService {
+  private readonly AUTH_KEY = 'auth';
+
+  setAuthState(authState: boolean): void {
+    localStorage.setItem(this.AUTH_KEY, JSON.stringify(authState));
+  }
+
+  getAuthState(): boolean {
+    const storedAuthState = localStorage.getItem(this.AUTH_KEY);
+    return storedAuthState ? JSON.parse(storedAuthState) : false;
+  }
+}
