6e08a2d6dcb844fd3d4de72f885d3e35aed0a44e
TheDollMaster98 <lorisdalsanto@hotmail.it>
2024-02-02 18:36:46 +0100

    cambio colore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,6 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { HttpClientModule } from '@angular/common/http';
 
-import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { getDatabase, provideDatabase } from '@angular/fire/database';
-
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { GlitchClockComponent } from './components/glitch-clock/glitch-clock.component';
 import { DashboardPage } from './components/pages/dashboard/dashboard.page';
@@ -17,16 +13,9 @@ import { DashboardService } from './service/dashboard.service';
 import { CarouselComponent } from './components/carousel/carousel.component';
 import { ComingsoonPage } from './components/pages/comingsoon/comingsoon.component';
 import { WhiteLogoComponent } from './components/icon-component/white-logo/white-logo.component';
-import { InstagramLinkComponent } from './components/icon-component/instagram-link/instagram-link.component';
-import { LinkedinLinkComponent } from './components/icon-component/linkedin-link/linkedin-link.component';
-import { TiktokLinkComponent } from './components/icon-component/tiktok-link/tiktok-link.component';
-import { YoutubeLinkComponent } from './components/icon-component/youtube-link/youtube-link.component';
-import { environment } from 'src/environments/environment';
 import { GdprComponent } from './components/privacy/gdpr/gdpr.component';
 import { PrivacyPolicyPage } from './components/privacy/privacy-policy/privacy-policy.component';
-import { ArticleEditorPage } from './components/editor/article-editor/article-editor.component';
 import { WhoarePage } from './components/pages/whoare/whoare.page';
-import { PlatformLocation } from '@angular/common';
 import { MockPlatformLocation } from '@angular/common/testing';
 import { CarouselSectionComponent } from './components/carousel-section/carousel-section.component';
 
